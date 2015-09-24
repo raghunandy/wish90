2a0282141577987c5c8f967c58663ee0361fedf7
Venky <venkateshpapineni@gmail.com>
2015-09-23 23:43:43 -0500

    facebook login success

--- Wish90App/android/mobile/src/main/java/wish90/android/gygafun/wish90/Wish90MainActivity.java
+++ Wish90App/android/mobile/src/main/java/wish90/android/gygafun/wish90/Wish90MainActivity.java
@@ -1,17 +1,123 @@
 package wish90.android.gygafun.wish90;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-public class Wish90MainActivity extends AppCompatActivity {
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+public class Wish90MainActivity extends Activity {
+    CallbackManager callbackManager;
+    LoginButton loginButton;
+    EditText x,y;
+    public static String App_Id;
+
+    static {
+        App_Id = "900075136727545";
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        FacebookSdk.setApplicationId(App_Id);
         setContentView(R.layout.activity_wish90_main);
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "wish90.android.gygafun.wish90",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+        callbackManager = CallbackManager.Factory.create();
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("public_profile","user_friends");
+        LoginManager lm=LoginManager.getInstance();
+        lm.registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+
+
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        Log.i("onsuccess", "Onsuccess");
+                        GraphRequest gr = GraphRequest.
+                                newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                                    @Override
+                                    public void onCompleted(JSONObject object, GraphResponse response) {
+
+                                        try {
+                                            String ursnme = object.getString("id");
+                                            String urs_bday = object.getString("user_birthday");
+                                            x = (EditText) findViewById(R.id.editText);
+                                            y = (EditText) findViewById(R.id.editText2);
+                                            x.setText(ursnme);
+                                            y.setText(urs_bday);
+                                        } catch (JSONException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+                                });
+
+                        Bundle parameters = new Bundle();
+                        parameters.putString("fields", "id,name,link");
+                        gr.setParameters(parameters);
+                        gr.executeAsync();
+                    }
+
+
+                    @Override
+                    public void onCancel() {
+                        // App code
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        Log.e("wish90", "here1", exception);
+                        // App co
+                    }
+                });
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fblogin();
+            }
+        });
     }
+        public void fblogin(){
+        LoginManager lm=LoginManager.getInstance();
+            lm.logInWithReadPermissions(this, Arrays.asList("public_profile", "user_friends"));
+
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -34,4 +140,9 @@ public class Wish90MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
 }
