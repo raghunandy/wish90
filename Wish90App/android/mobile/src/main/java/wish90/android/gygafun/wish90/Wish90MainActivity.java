4fa5bd5a0f98ea008130e9e625f7e1ea77dacfb7
DeeptiBhatia2906 <deepti.sbhatia@gmail.com>
2015-10-04 02:43:44 -0500

    Basic module to create a notification

--- Wish90App/android/mobile/src/main/java/wish90/android/gygafun/wish90/Wish90MainActivity.java
+++ Wish90App/android/mobile/src/main/java/wish90/android/gygafun/wish90/Wish90MainActivity.java
@@ -1,37 +1,36 @@
 package wish90.android.gygafun.wish90;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.NotificationCompat;
+import android.view.View;
 
 public class Wish90MainActivity extends AppCompatActivity {
 
+    NotificationCompat.Builder notification;
+    private static final int uniqueID=23;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wish90_main);
+        notification=new NotificationCompat.Builder(this);
+        notification.setAutoCancel(true);
     }
+    public void notificationButtonClick(View view){
+        notification.setSmallIcon(R.mipmap.ic_launcher);
+        notification.setTicker("This is the ticker..........");
+        notification.setWhen(System.currentTimeMillis());
+        notification.setContentTitle("Here is the title");
+        notification.setContentText("I am the body text of your notification");
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_wish90_main, menu);
-        return true;
-    }
+        Intent intent=new Intent(this, Wish90MainActivity.class);
+        PendingIntent pendingIntent= PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+        notification.setContentIntent(pendingIntent);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        NotificationManager nm=(NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        nm.notify(uniqueID,notification.build());
     }
 }
